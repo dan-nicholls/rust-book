4c98a44c1f8b0a0bf81491ec67ebb5d0020b6bf5
Dan Nicholls <dan.nicholls@trurecognition.io>
2024-03-03 18:15:33 +1100

    Add functions examples from chapter 3

---
+++ 3/functions/src/main.rs
@@ -0,0 +1,40 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+    print_labeled_measurement(5, 'm');
+
+    // Expressions
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
+
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn another_function(x: i32) {
+    println!("Another function.");
+    println!("the value of x is {x}")
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    // Expression is only returned when no ; is available.
+    // x + 1;
+    x + 1
+}
